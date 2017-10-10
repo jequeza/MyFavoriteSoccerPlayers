1a011bf33b69c9a89621788f3ebe9a7d00e13fa1
Jesus Quezada <maj3st1@gmail.com>
2017-10-09 21:55:27 -0500

    JQ

--- MyFavoriteSoccerPlayers/DetailViewController.swift
+++ MyFavoriteSoccerPlayers/DetailViewController.swift
@@ -9,37 +9,41 @@
 import UIKit
 
 class DetailViewController: UIViewController {
-
-    @IBOutlet weak var detailDescriptionLabel: UILabel!
-
-
+    
+    //   @IBOutlet weak var detailDescriptionLabel: UILabel!
+    
+    @IBOutlet weak var productImageView: UIImageView!
+    
+    
+    
     func configureView() {
         // Update the user interface for the detail item.
-        if let detail = detailItem {
-            if let label = detailDescriptionLabel {
-                label.text = detail.description
+        if detailItem != nil {
+            if let photo = productImageView {
+                photo.image = UIImage(named:detailItem!)
             }
+        }else{
+            productImageView.image = UIImage(named:"Ronaldinho.jpg")
         }
     }
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         configureView()
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-    var detailItem: NSDate? {
+    
+    var detailItem: String? {
         didSet {
             // Update the view.
             configureView()
         }
     }
-
-
+    
+    
 }
-
--- MyFavoriteSoccerPlayers/MasterViewController.swift
+++ MyFavoriteSoccerPlayers/MasterViewController.swift
@@ -9,87 +9,91 @@
 import UIKit
 
 class MasterViewController: UITableViewController {
-
+    
     var detailViewController: DetailViewController? = nil
-    var objects = [Any]()
-
-
+    //var objects = [Any]()
+    var listArray = NSMutableArray()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        navigationItem.leftBarButtonItem = editButtonItem
-
-        let addButton = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(insertNewObject(_:)))
-        navigationItem.rightBarButtonItem = addButton
+        //        navigationItem.leftBarButtonItem = editButtonItem
+        //
+        //        let addButton = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: ////#selector(insertNewObject(_:)))
+        //        navigationItem.rightBarButtonItem = addButton
+        listArray = [ "Bastian Schweinsteiger","Cristiano Ronaldo","Cuauhtemoc Blanco","Lionel Messi","Petr Cech","Ronaldinho","Ronaldo Nazario","Thierry Henry","Gianluigi Buffon","Zlatan Ibrahimovic"]
+        title = "My Favorite Soccer Players"
         if let split = splitViewController {
             let controllers = split.viewControllers
             detailViewController = (controllers[controllers.count-1] as! UINavigationController).topViewController as? DetailViewController
         }
     }
-
+    
     override func viewWillAppear(_ animated: Bool) {
         clearsSelectionOnViewWillAppear = splitViewController!.isCollapsed
         super.viewWillAppear(animated)
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-    @objc
-    func insertNewObject(_ sender: Any) {
-        objects.insert(NSDate(), at: 0)
-        let indexPath = IndexPath(row: 0, section: 0)
-        tableView.insertRows(at: [indexPath], with: .automatic)
-    }
-
+    
+    //    @objc
+    //    func insertNewObject(_ sender: Any) {
+    //        objects.insert(NSDate(), at: 0)
+    //        let indexPath = IndexPath(row: 0, section: 0)
+    //        tableView.insertRows(at: [indexPath], with: .automatic)
+    //    }
+    
     // MARK: - Segues
-
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "showDetail" {
             if let indexPath = tableView.indexPathForSelectedRow {
-                let object = objects[indexPath.row] as! NSDate
+                let object = listArray[indexPath.row] as! String
                 let controller = (segue.destination as! UINavigationController).topViewController as! DetailViewController
-                controller.detailItem = object
+                controller.detailItem = (object as String) + ".jpg"
                 controller.navigationItem.leftBarButtonItem = splitViewController?.displayModeButtonItem
                 controller.navigationItem.leftItemsSupplementBackButton = true
+                self.splitViewController?.preferredDisplayMode = .primaryHidden
             }
         }
     }
-
+    
     // MARK: - Table View
-
+    
     override func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return objects.count
+        return listArray.count
     }
-
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-
-        let object = objects[indexPath.row] as! NSDate
+        
+        let object = listArray[indexPath.row] as! NSString
         cell.textLabel!.text = object.description
         return cell
     }
-
+    
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
         // Return false if you do not want the specified item to be editable.
-        return true
+        return false
     }
-
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            objects.remove(at: indexPath.row)
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view.
-        }
-    }
-
-
+    
+    //    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+    //        if editingStyle == .delete {
+    //            objects.remove(at: indexPath.row)
+    //            tableView.deleteRows(at: [indexPath], with: .fade)
+    //        } else if editingStyle == .insert {
+    //            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view.
+    //        }
+    //    }
+    
+    
 }
 
